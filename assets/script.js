2f490654e538087d18e5c76f80f67aba1e6dbb71
Pedro <pkcarreno@gmail.com>
2019-04-09 20:40:56 -0400

    retomando funciones del js
    determinando minimos y maximo, accion empezada

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,19 @@
 function onFullCharge(){
 
   var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-
-  /*initialSettings();
+  
+  initialSettings();
 
   function initialSettings(){
     //a la espera de algun ajuste inicial
-  }*/
+    document.getElementById("headerTitle").style.height = height + "vh";
+  }
 
   document.onscroll = function(){
     var scrollTop = document.documentElement.scrollTop || document.body;
     var pixels = scrollTop / 70;
     var header = document.getElementById("header");
-  
+
     if(scrollTop < height){
       header.style.filter = "blur(" + pixels + "px)";
       header.style.WebkitFilter = "blur(" + pixels + "px)";
