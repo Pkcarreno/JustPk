2ec96992fd1afc85f4f758c82d90044f1ec34a52
Pedro <pkcarreno@gmail.com>
2019-04-20 17:14:37 -0400

    -js optimizado scroll
    -falta condicional para parar la ejecucion

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,16 @@
 function onFullCharge() {
-  window.onscroll = function() {
-    headerblur();
-  };
+  window.addEventListener('scroll', function (e) {
+    headerblur()
+  });
 }
+
 function headerblur() {
-  var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-  var scrollTop = document.documentElement.scrollTop || document.body;
-  var pixels = scrollTop / 70;
-  var header = document.getElementById("header");
-  
-  if (scrollTop < height) {
-    header.style.filter = "blur(" + pixels + "px)";
-    header.style.WebkitFilter = "blur(" + pixels + "px)";
-    header.style.backgroundPosition = "center -" + pixels * 15 + "px";
-  }
-}
-function throttle(fn, wait) {
-  var time = Date.now();
-  return function () {
-    if ((time + wait - Date.now()) < 0) {
-      fn();
-      time = Date.now();
-    }
-  }
+    const target = document.querySelector('header');
+    var scrolled = window.pageYOffset;
+    var rate = scrolled * 0.05;
+
+    console.log(scrolled);
+
+    target.style.backgroundPosition = "center -" + rate + "px";
+    target.style.filter = "blur(" + rate + "px)";
 }
