230c7ae35dcf66c3d249b9e39464c859cf8dfd12
Pedro <pkcarreno@gmail.com>
2019-04-20 14:11:18 -0400

    -js ordenado
    -en proceso de optimizar el js en el movimiento del dom

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,26 @@
-function onFullCharge(){
-
-  var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-  var titleScrollTopBuffer;
-    var titlePixels;
- /* initialSettings();
-
-  function initialSettings(){
-    //a la espera de algun ajuste inicial
-    document.getElementById("headerTitle").style.height = height + "vh";
-  }*/
-
-  document.onscroll = function(){
-    var scrollTop = document.documentElement.scrollTop || document.body;
-    var scrollDown = document.documentElement.scrollDown ;
-    var pixels = scrollTop / 70;
-    var header = document.getElementById("header");
-
-    var title = document.getElementById("title");
-    var titleleft = document.getElementById("leftTitle");
-    var titleScrollTop = titleleft.scrollTop;
-    
-
-    if(scrollTop < height){
-      header.style.filter = "blur(" + pixels + "px)";
-      header.style.WebkitFilter = "blur(" + pixels + "px)";
-      header.style.backgroundPosition = "center -" + pixels * 15 + "px";
-      console.log(titlePixels);
-
-      if(scrollTop > scrollDown){
-        titlePixels += 1;
-
-      }else{
-        titlePixels -= 1;
-
-      }
-      title.style.marginTop = titlePixels + "%";
-      
-    }
+function onFullCharge() {
+  window.onscroll = function() {
+    headerblur();
   };
+}
+function headerblur() {
+  var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  var scrollTop = document.documentElement.scrollTop || document.body;
+  var pixels = scrollTop / 70;
+  var header = document.getElementById("header");
+  
+  if (scrollTop < height) {
+    header.style.filter = "blur(" + pixels + "px)";
+    header.style.WebkitFilter = "blur(" + pixels + "px)";
+    header.style.backgroundPosition = "center -" + pixels * 15 + "px";
+  }
+}
+function throttle(fn, wait) {
+  var time = Date.now();
+  return function () {
+    if ((time + wait - Date.now()) < 0) {
+      fn();
+      time = Date.now();
+    }
+  }
 }
