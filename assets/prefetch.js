080eaa3a5bfba8953ef36aa6c9233db3eddfa233
Pedro <pkcarreno@gmail.com>
2019-05-01 01:14:49 -0400

    optimizacion en prefetch.js
    cambio de fuentes

--- assets/prefetch.js
+++ assets/prefetch.js
@@ -1,22 +1,33 @@
 function setIni() {
-  setImage("Chrome 32 Firefox 65 Opera 19 Edge 18");
+  navAndVersionCheck("Chrome 32 Firefox 65 Opera 19 Edge 18",setImage);
 }
-function setImage(text) {
-  const element_1 = document.querySelector("header");
-  const element_2 = document.getElementById("headerMe");
-  let word = text.split(' ');
+function navAndVersionCheck() {
+  let word = arguments[0].split(' ');
   let navVersionString = navigator.appVersion.split(' ');
-  console.log("hola");
+  let bol = false;
   for (let i = 0; i < (word.length - 1); i += 2) {
     let version = 0;
     for (let b = 0; b < navVersionString.length; b++) {
       if (word[i] == navVersionString[b].substring(word[i].length, -1))
-        version = navVersionString[b].substring(word[i].length + 1);
+      version = navVersionString[b].substring(word[i].length + 1);
     }
     if ((navigator.userAgent.indexOf(word[i]) > -1) && (word[i + 1] <= version)) {
-      element_1.style.backgroundImage = "url('./assets/pictures/headerBack-4.webp')";
-      element_2.style.backgroundImage = "url('./assets/pictures/headerMe-4.webp')";
-      return true;
+      if (arguments[1]!=undefined) {
+        arguments[1]();
+      }
+      console.log("hola")
+      bol = true;
+      nav = word[i];
+      ver = version;
+      return{
+        bol,
+        nav,
+        ver
+      }
     }
   }
+}
+function setImage() {
+  document.querySelector("header").style.backgroundImage = "url('./assets/pictures/headerBack-4.webp')";
+  document.getElementById("headerMe").style.backgroundImage = "url('./assets/pictures/headerMe-4.webp')";
 }
