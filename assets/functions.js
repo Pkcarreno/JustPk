674dd3acbb310e3dc0e5ee64829817a49d8d7978
Pedro <pkcarreno@gmail.com>
2019-05-02 01:07:05 -0400

    minnor fixed

--- assets/functions.js
+++ assets/functions.js
@@ -6,8 +6,9 @@ function navAndVersionCheck() {
   for (let i = 0; i < (word.length - 1); i += 2) {
     let version = 0;
     for (let b = 0; b < navVersionString.length; b++) {
-      if (word[i] == navVersionString[b].substring(word[i].length, -1))
+      if (word[i] == navVersionString[b].substring(word[i].length, -1)){
         version = navVersionString[b].substring(word[i].length + 1);
+      }
     }
     if ((navigator.userAgent.indexOf(word[i]) > -1) && (word[i + 1] <= version)) {
       if (arguments[1] != undefined) {
