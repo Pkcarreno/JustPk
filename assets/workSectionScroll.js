6c2eae88b35cb0b6d2dc0c7df76e379f0ac60ba9
Pedro <pkcarreno@gmail.com>
2019-05-12 13:02:10 -0400

    menor fixes
    add button events

---
+++ assets/workSectionScroll.js
@@ -0,0 +1,6 @@
+function leftScroll(target, range) {
+  document.getElementById(target).scrollLeft -= range;
+}
+function rightScroll(target, range) {
+  document.getElementById(target).scrollLeft += range;
+}
