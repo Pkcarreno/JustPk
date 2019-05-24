5fe4c932143658c276b2e3ea8c7d71fcb65d4b4d
Pedro <pkcarreno@gmail.com>
2019-05-24 01:45:17 -0400

    only left the laguaje nav an spanish version but that are a mantequilla chico

--- assets/textAdd.js
+++ assets/textAdd.js
@@ -1,11 +1,38 @@
 function init() {
-  const subtitle = (
+  var words = [
     'Software developer',
     '(Soon) System engineer',
     'Game developer enthusiast'
-  );
-  
-}
-function addText(var_list) {
-  
+  ],
+    wordWrapper = document.getElementById('subtitleText'),
+    wordWrapperContent = wordWrapper.innerHTML,
+    addingWord = false,
+    counter = 1;
+
+  setInterval(function () {
+
+    if (wordWrapperContent.length > 0 && !addingWord) {
+      wordWrapper.innerHTML = wordWrapperContent.slice(0, -1);
+      wordWrapperContent = wordWrapper.innerHTML;
+    } else {
+      addingWord = true;
+    }
+
+    if (addingWord) {
+      if (wordWrapperContent.length < words[counter].length) {
+        wordWrapper.innerHTML = words[counter].slice(0, wordWrapperContent.length + 1);
+        wordWrapperContent = wordWrapper.innerHTML;
+      } else {
+        if (counter < words.length) {
+          counter++
+        }
+        addingWord = false;
+      }
+    }
+
+    if (counter == words.length) {
+      counter = 0;
+    }
+
+  }, 150);
 }
