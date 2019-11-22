956055586667c1d746214845b916ef3b9ffb9056
Pedro Carreño <pkcarreno@gmail.com>
2019-11-22 14:40:43 -0400

    animaciones en context puesta version final lista

--- src/App.js
+++ src/App.js
@@ -127,7 +127,7 @@ class Context extends Component {
       if (isMobile) {
         return(
           <React.Fragment>
-            <button className='button' onClick={e => { toggle('menu')}}>
+            <button className='button animationInOut' onClick={ e => { toggle('menu')}}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
             </button>
           </React.Fragment>
@@ -216,7 +216,6 @@ class App extends Component {
     });
   }
   toggleContext = (i) => {
-    setTimeout(() => {}, 700);
     this.setState({
       show: i
     })
